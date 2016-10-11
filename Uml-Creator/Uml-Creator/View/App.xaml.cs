4c3a71ea0b8a897e815b7f5fbf7223865507dfdc
Martin Vieth <martin_123_vieth@hotmail.com>
2016-10-11 10:48:35 +0200

    Tilføjet mapper til View og ViewModel og rykket MainWindow

---
+++ Uml-Creator/Uml-Creator/View/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Uml_Creator.View
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// 
+    /// test
+    public partial class App : Application
+    {
+    }
+}
---
+++ Uml-Creator/Uml-Creator/View/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Uml_Creator.View
+{
+    //
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
