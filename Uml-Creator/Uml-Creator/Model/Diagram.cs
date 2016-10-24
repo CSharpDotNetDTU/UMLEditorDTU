e8c575d42c841968dc3426395eb226b3887d75fb
Martin Vieth <martin_123_vieth@hotmail.com>
2016-10-24 14:05:04 +0200

    Lavet Interfaces til figurer og linjer, samt model til at indeholde figurer og linjer

---
+++ Uml-Creator/Uml-Creator/Model/Diagram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uml_Creator.Model.Interfaces;
+
+namespace Uml_Creator.Model
+{
+    public class Diagram
+    {
+        public List<IFigure> Shapes { get; set; }
+        public List<ILine> Lines { get; set; }
+    }
+}
---
+++ Uml-Creator/Uml-Creator/Model/Interfaces/IFigure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uml_Creator.Model.ENUM;
+
+namespace Uml_Creator.Model.Interfaces
+{
+    public interface IFigure
+    {
+        EFigure Type { get; }
+        double Height { get; set; }
+        double Width { get; set; }
+        int FigureNr { get; }
+        double X { get; }
+        double Y { get; }
+        string Data { get; set; }
+        string ToString();
+
+
+    }
+}
