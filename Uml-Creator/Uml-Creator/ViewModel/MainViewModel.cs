e6feff3a28b90f7eca095e063158ba9213d37230
zanakis <wihte@DESKTOP-JR1U121>
2016-12-07 23:56:34 +0100

    export med hotkeys virker, ikke med knap export virker ikke i thread

--- Uml-Creator/Uml-Creator/ViewModel/MainViewModel.cs
+++ Uml-Creator/Uml-Creator/ViewModel/MainViewModel.cs
@@ -82,12 +82,15 @@ namespace Uml_Creator.ViewModel
         public ICommand RedoCommand { get; }
         public ICommand AddCommand { get; }
         public ICommand BtnAddClass { get; }
+        public ICommand DeleteCommand { get; }
         UndoRedoController undoRedoController = UndoRedoController.Instance;
         public ObservableCollection<LineViewModel> lines { get; }
         public bool isAddingLineBtnPressed;
         public FigureViewModel _firstShapeToConnect;
         public string FileName;
+        public string FileNamePic;
         BackgroundWorker worker = new BackgroundWorker();
+        public Grid canvas;
 
         public MainViewModel()
         {
@@ -122,7 +125,6 @@ namespace Uml_Creator.ViewModel
             RedoCommand = new RelayCommand(undoRedoController.Redo, undoRedoController.canRedo);
             AddCommand = new RelayCommand(AddFigure);
             BtnAddClass = new RelayCommand(AddClass);
-
 
 
         }
@@ -251,44 +253,6 @@ namespace Uml_Creator.ViewModel
             loadfildialog.Filter = "XML files (*.xml)|*.xml";
             if (loadfildialog.ShowDialog() != DialogResult.OK) return;
             FileName = loadfildialog.FileName;
-            //worker.DoWork += worker_Load;
-            //worker.RunWorkerAsync();
-            try
-            {
-                XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument.Load(FileName);
-                string xmlString = xmlDocument.OuterXml;
-
-                using (StringReader read = new StringReader(xmlString))
-                {
-                    ObservableCollection<FigureViewModel> temp;
-                    Type outType = typeof(ObservableCollection<FigureViewModel>); //skal være samme slags objekter som diagrammet
-                    XmlSerializer serializer = new XmlSerializer(outType);
-                    using (XmlReader reader = new XmlTextReader(read))
-                    {
-                        temp = (ObservableCollection<FigureViewModel>)serializer.Deserialize(reader);
-                        FiguresViewModels.Clear();
-                        for (int i = 0; i < temp.Count; i++)
-                        {
-                            //  FiguresViewModels.Add(new FigureViewModel(temp[i].X, temp[i].Y, temp[i].Width, temp[i].Height, temp[i].Data, temp[i].Type,false,temp[i].Name));
-                            FiguresViewModels.Add(new FigureViewModel(temp[i]));
-                        }
-
-                        reader.Close();
-                    }
-
-                    read.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                //Log exception here
-            }
-        }
-
-        private void worker_Load(object sender, DoWorkEventArgs e)
-        {
             try
             {
                 XmlDocument xmlDocument = new XmlDocument();
@@ -360,27 +324,45 @@ namespace Uml_Creator.ViewModel
 
         private void Export_Click(Grid canvas)
         {
-            
+            this.canvas = canvas;
             SaveFileDialog exportfildialog = new SaveFileDialog();
             exportfildialog.Filter = "PNG files (*.png)|*.png";
 
-            if (exportfildialog.ShowDialog() == DialogResult.OK)
+            if (!(exportfildialog.ShowDialog() == DialogResult.OK)) return;
+            FileNamePic = exportfildialog.FileName;
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)canvas.RenderSize.Width,
+                                           (int)canvas.RenderSize.Height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(canvas);
+
+            //var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, 1000, 1000));
+
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+            using (var fs = File.OpenWrite(FileNamePic))
             {
-                RenderTargetBitmap rtb = new RenderTargetBitmap((int)canvas.RenderSize.Width,
-                                            (int)canvas.RenderSize.Height, 96d, 96d, PixelFormats.Default);
-                rtb.Render(canvas);
-
-                //var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, 1000, 1000));
-
-                BitmapEncoder pngEncoder = new PngBitmapEncoder();
-                pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
-
-                using (var fs = File.OpenWrite(exportfildialog.FileName))
-                {
-                    pngEncoder.Save(fs);
-                }
+                pngEncoder.Save(fs);
             }
+            //worker.DoWork += worker_Export;
+            //worker.RunWorkerAsync();
+        }
+
+       /* private void worker_Export(object sender, DoWorkEventArgs e)
+        {
+            Grid canvas = this.canvas;
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)canvas.RenderSize.Width,
+                                           (int)canvas.RenderSize.Height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(canvas);
+
+            //var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, 1000, 1000));
+
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+            using (var fs = File.OpenWrite(FileNamePic))
+            {
+                pngEncoder.Save(fs);
             }
+        }*/
 
 
         public ObservableCollection<FigureViewModel> FigureViewModels
